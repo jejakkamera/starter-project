ca818a434b728e53bf96de5ff065b1733679cf57
adirp <kurcaci507@gmail.com>
2021-06-29 14:04:35 +0700

    first commit

---
+++ application/logs/log-2021-06-29.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2021-06-29 14:02:52 --> Config Class Initialized
+INFO - 2021-06-29 14:02:52 --> Hooks Class Initialized
+DEBUG - 2021-06-29 14:02:52 --> UTF-8 Support Enabled
+INFO - 2021-06-29 14:02:52 --> Utf8 Class Initialized
+INFO - 2021-06-29 14:02:52 --> URI Class Initialized
+DEBUG - 2021-06-29 14:02:52 --> No URI present. Default controller set.
+INFO - 2021-06-29 14:02:52 --> Router Class Initialized
+INFO - 2021-06-29 14:02:52 --> Output Class Initialized
+INFO - 2021-06-29 14:02:52 --> Security Class Initialized
+DEBUG - 2021-06-29 14:02:52 --> Global POST, GET and COOKIE data sanitized
+INFO - 2021-06-29 14:02:52 --> Input Class Initialized
+INFO - 2021-06-29 14:02:52 --> Language Class Initialized
+INFO - 2021-06-29 14:02:52 --> Loader Class Initialized
+INFO - 2021-06-29 14:02:52 --> Helper loaded: url_helper
+INFO - 2021-06-29 14:02:52 --> Helper loaded: security_helper
+INFO - 2021-06-29 14:02:52 --> Database Driver Class Initialized
+ERROR - 2021-06-29 14:02:56 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ E:\folder xampp\xampp7\htdocs\project\main_rosma\alumni\starter-project\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-06-29 14:02:56 --> Unable to connect to the database
+INFO - 2021-06-29 14:02:56 --> Language file loaded: language/english/db_lang.php
